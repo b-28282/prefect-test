394672ae670b9d003404283c42a37d7701a3412f
Jeff Hale <jeffmshale@gmail.com>
2023-12-19 15:26:24 -0500

    add qs

---
+++ my_workflow.py
@@ -0,0 +1,15 @@
+import httpx
+from prefect import flow
+
+
+@flow(log_prints=True)
+def get_repo_info():
+    url = "https://api.github.com/repos/PrefectHQ/prefect"
+    response = httpx.get(url)
+    repo = response.json()
+    print("PrefectHQ/prefect repository statistics 🤓:")
+    print(f"Stars 🌠 : {repo['stargazers_count']}")
+
+
+if __name__ == "__main__":
+    get_repo_info()
