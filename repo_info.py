b0d910bbfbf662a806745d5b2ea6e487e562962b
Jeff Hale <jeffmshale@gmail.com>
2023-12-21 13:11:11 -0500

    update for schedule

--- repo_info.py
+++ repo_info.py
@@ -3,21 +3,20 @@ from prefect import flow
 
 
 @flow(log_prints=True)
-def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
-    url = f"https://api.github.com/repos/{repo_name}"
+def get_repo_info():
+    url = "https://api.github.com/repos/PrefectHQ/prefect"
     response = httpx.get(url)
-    response.raise_for_status()
     repo = response.json()
-    print(f"{repo_name} repository statistics 🤓:")
+    print("PrefectHQ/prefect repository statistics 🤓:")
     print(f"Stars 🌠 : {repo['stargazers_count']}")
-    print(f"Forks 🍴 : {repo['forks_count']}")
 
 
 if __name__ == "__main__":
-    get_repo_info.from_source(
-        source="https://github.com/discdiver/demos.git",
-        entrypoint="repo_info.py:get_repo_info",
+    flow.from_source(
+        source="https://github.com/discdiver/demo.git",
+        entrypoint="my_workflow.py:get_repo_info",
     ).deploy(
         name="my-first-deployment",
         work_pool_name="my-managed-pool",
+        cron="0 1 * * *",
     )
