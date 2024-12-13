6a56efad773095825ead037a9ad6d27e590ffd0a
Daniel Sauble <djsauble@gmail.com>
2024-12-13 10:41:31 -0800

    See if the problem is due to missing dependencies

--- train_model.py
+++ train_model.py
@@ -1,47 +1,47 @@
-from pathlib import Path
-from prefect import flow, task
-from prefect_aws import S3Bucket
-from datetime import datetime
-from prefect.artifacts import create_markdown_artifact
+# from pathlib import Path
+from prefect import flow#, task
+# from prefect_aws import S3Bucket
+# from datetime import datetime
+# from prefect.artifacts import create_markdown_artifact
 
-@task(log_prints=True)
-def train_model(path: Path) -> object:
-    print("Loading downloaded file…")
-    with open(path, 'r') as file:
-        contents = file.read()
+# @task(log_prints=True)
+# def train_model(path: Path) -> object:
+#     print("Loading downloaded file…")
+#     with open(path, 'r') as file:
+#         contents = file.read()
     
-    results = {
-        "timestamp": datetime.now().isoformat(),
-        "contents": contents
-    }
+#     results = {
+#         "timestamp": datetime.now().isoformat(),
+#         "contents": contents
+#     }
     
-    return results
+#     return results
 
-@task(log_prints=True)
-def save_results(results: object):
-    print("Saving results as artifact…")
-    markdown_content = f"""
-## Model Training Results
+# @task(log_prints=True)
+# def save_results(results: object):
+#     print("Saving results as artifact…")
+#     markdown_content = f"""
+# ## Model Training Results
 
-{results['timestamp']}
+# {results['timestamp']}
 
-{results['contents']}
-    """
-    create_markdown_artifact(
-        key="model-metrics",
-        markdown=markdown_content,
-        description="Training metrics for the linear regression model"
-    )
+# {results['contents']}
+#     """
+#     create_markdown_artifact(
+#         key="model-metrics",
+#         markdown=markdown_content,
+#         description="Training metrics for the linear regression model"
+#     )
 
-@task(log_prints=True)
-def fetch_updated_data(path: Path):
-    s3_bucket = S3Bucket.load("s3-bucket-block")
-    try:
-        print(f"Fetching {path} from S3…")
-        filepath = s3_bucket.download_object_to_path(path, path)
-        print(f"File downloaded to: {filepath}")
-    except Exception as e:
-        raise Exception(f"Failed to fetch {path} from S3: {e}")
+# @task(log_prints=True)
+# def fetch_updated_data(path: Path):
+#     s3_bucket = S3Bucket.load("s3-bucket-block")
+#     try:
+#         print(f"Fetching {path} from S3…")
+#         filepath = s3_bucket.download_object_to_path(path, path)
+#         print(f"File downloaded to: {filepath}")
+#     except Exception as e:
+#         raise Exception(f"Failed to fetch {path} from S3: {e}")
 
 @flow(log_prints=True)
 def update_model():
