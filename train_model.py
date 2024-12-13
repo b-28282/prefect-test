134666500a68d818526c8d6595326934a20e14a2
Daniel Sauble <djsauble@gmail.com>
2024-12-13 10:56:33 -0800

    Try the original code again

--- train_model.py
+++ train_model.py
@@ -1,55 +1,49 @@
 from pathlib import Path
-from prefect import flow#, task
+from prefect import flow, task
 from prefect_aws import S3Bucket
 from datetime import datetime
 from prefect.artifacts import create_markdown_artifact
 
-# @task(log_prints=True)
-# def train_model(path: Path) -> object:
-#     print("Loading downloaded file…")
-#     with open(path, 'r') as file:
-#         contents = file.read()
+@task
+def train_model(path: Path) -> object:
+    with open(path, 'r') as file:
+        contents = file.read()
     
-#     results = {
-#         "timestamp": datetime.now().isoformat(),
-#         "contents": contents
-#     }
+    results = {
+        "timestamp": datetime.now().isoformat(),
+        "contents": contents
+    }
     
-#     return results
+    return results
 
-# @task(log_prints=True)
-# def save_results(results: object):
-#     print("Saving results as artifact…")
-#     markdown_content = f"""
-# ## Model Training Results
+@task
+def save_results(results: object):
+    markdown_content = f"""
+## Model Training Results
 
-# {results['timestamp']}
+{results['timestamp']}
 
-# {results['contents']}
-#     """
-#     create_markdown_artifact(
-#         key="model-metrics",
-#         markdown=markdown_content,
-#         description="Training metrics for the linear regression model"
-#     )
+{results['contents']}
+    """
+    create_markdown_artifact(
+        key="model-metrics",
+        markdown=markdown_content,
+        description="Training metrics for the linear regression model"
+    )
 
-# @task(log_prints=True)
-# def fetch_updated_data(path: Path):
-#     s3_bucket = S3Bucket.load("s3-bucket-block")
-#     try:
-#         print(f"Fetching {path} from S3…")
-#         filepath = s3_bucket.download_object_to_path(path, path)
-#         print(f"File downloaded to: {filepath}")
-#     except Exception as e:
-#         raise Exception(f"Failed to fetch {path} from S3: {e}")
+@task
+def fetch_updated_data(path: Path):
+    s3_bucket = S3Bucket.load("s3-bucket-block")
+    try:
+        filepath = s3_bucket.download_object_to_path(path, path)
+    except Exception as e:
+        raise Exception(f"Failed to fetch {path} from S3: {e}")
 
-@flow(log_prints=True)
-def update_model():
-    # path = Path("test.txt")
-    # fetch_updated_data(path)
-    # results = train_model(path)
-    # save_results(results)
-    print("Hello World")
+@flow
+def update_model(path: Path):
+    fetch_updated_data(path)
+    results = train_model(path)
+    save_results(results)
 
 if __name__ == "__main__":
     flow.from_source(
@@ -58,5 +52,4 @@ if __name__ == "__main__":
     ).deploy(
         name="webhook-test",
         work_pool_name="my-managed-pool",
-        cron="0 1 * * *",
     )
